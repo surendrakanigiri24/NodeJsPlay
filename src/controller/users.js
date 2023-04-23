316e2057fe5606bf6993d2f281ce9ec3f11e2fdf
surendrakanigiri24 <surendrakanigiri24@gmail.com>
2023-04-23 11:21:25 +0530

    Implemented Joi validator

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,10 +1,12 @@
-const manager = require("../managers/index")
+const manager = require("../managers/index");
+const validator = require("../validators/users/userRegValidator");
 
 
 const Users = {
 
     // User registration to the application
     userRegistration : async (req,res) => {
+        validator.validateAsync(req.body);
         const newUserCreated = await manager.userManager.userCreation(req.body);
         res.json(newUserCreated);
     }
