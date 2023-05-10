495ed648981f626be20592453f2de28abcb02ef9
surendrakanigiri24 <surendrakanigiri24@gmail.com>
2023-05-10 10:17:06 +0530

    Created basics for chai tests

---
+++ src/test/baseTestStuff.js
@@ -0,0 +1,25 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = "test";
+
+// Test dev dependency setup:
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+chai.should();
+
+
+const TEST_DATA = {
+    USER : {
+        "email": "siriv1997@gmail.net",
+        "password": "surendraKanigiri",
+        "firstName": "Surendra",
+        "lastName": "Kanigiri",
+        "role": "user",
+        "isVerified": false
+    }
+}
+
+module.exports = {
+    chai,
+    TEST_DATA
+}
