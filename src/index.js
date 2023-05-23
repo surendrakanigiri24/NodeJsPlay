150b5b2fa5a5402fcf265d756d635f0983efdb64
surendrakanigiri24 <surendrakanigiri24@gmail.com>
2023-05-23 12:42:52 +0530

    To check ci/cd

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use("/api/users", routes.userRoutes);
 
 // sample route
 app.get("/", (req,res) => {
-  res.status(200).send({message:" YA!, I am workin"});
+  res.status(200).send({message:" YA!, I am working"});
 })
 
 // Swagger implementation
